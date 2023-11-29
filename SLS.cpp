c87f5e7e47fc059b7cf584c244e2ccacc5825349
esraamahmoud7 <101475325+esraamahmoud7@users.noreply.github.com>
2023-11-29 22:27:33 +0200

    Update SLS.cpp

--- SLS.cpp
+++ SLS.cpp
@@ -156,172 +156,197 @@ void SLS::deleteAuthor(Author A)
     else
         cout<<"This author doesn't exist\n";
 }
-void SLS::addAuthor(Author auhor)
+void SLS::Search_By_Primary(int Id)
 {
-    cout << auhor.ID << " |" << auhor.name;
-    string id = string(auhor.ID);
-    int check = binarySearch(authorIds , stoi(id));
-    if( check != -1)
+    store_Index_In_Map();
+    auto check = indexing.find(Id);
+    if (check != indexing.end()) {
+        authorFile.open("file.txt", ios::in);
+        long ByteOffset=indexing[Id];
+        authorFile.seekg(ByteOffset,ios::beg);
+        //don't want print without it
+        authorFile.get();
+        string line;
+        getline(authorFile, line);
+        cout<<"the Author Founded.. \n";
+//        cout<<line<<endl;
+        istringstream ss(line);
+        string token;
+        vector<std::string> tokens;
+        while (std::getline(ss, token, '|')) {
+            tokens.push_back(token);
+        }
+        cout<<"Author's ID : "<<tokens[0]<<endl;
+        cout<<"Author's name : "<<tokens[1]<<endl;
+        cout<<"Author's Address : "<<tokens[2]<<endl;
+    }
+    else
     {
-        return;
+        cout<<"Sorry this author not exist\n";
     }
-    authorFile.open("file.txt" , ios::app);
-    authorFile.write((char*)author.ID , strlen(author.ID));
-    authorFile << '|';
-    authorFile.write((char *) author.name , strlen(author.name));
-    authorFile << '|';
-    authorFile.write((char *) author.address , strlen(author.address));
-    authorFile << "\n";
-    insertAuthorIndex(recsize , author.ID);
-    recsize+=strlen(author.ID) + strlen(author.name) + strlen(author.address) + 3;
-    authorFile.close();
 }
-void SLS::addBook()
-{
-    bookFile.open("Books.txt", ios::out | ios:: binary | ios::app | ios::in);
-    // Check if the file is open
-    if (!bookFile.is_open()) {
-        cout << "Error opening file\n";
-        return;
-    }
-
-//    if (bookFile.peek() == EOF)
+//void SLS::addAuthor(Author auhor)
+//{
+//    cout << auhor.ID << " |" << auhor.name;
+//    string id = string(auhor.ID);
+//    int check = binarySearch(authorIds , stoi(id));
+//    if( check != -1)
 //    {
-//        char* size = "0";
-//        char* AVAIL = "-1";
-//        bookFile.write(size,sizeof (size));
-//        bookFile << "|";
-//        bookFile.write(AVAIL,sizeof (AVAIL));
-//        bookFile << "\n";
-//
+//        return;
 //    }
-
-    char isbn[15], t[30], aid[15];
-    cin.ignore();
-    cout << "Enter book's ISBN:";
-    cin >> isbn;
-    cout << "Enter book's title:";
-    cin >> t;
-    cout << "Enter book's author's ID:";
-    cin >> aid;
-
-    this->book = Book(isbn, t, aid);
-
-    // Write data as binary structure
-    bookFile.seekp(0, ios::end);
-    bookFile.write((char*)(&book), sizeof(book));
-
-
-    int newSize = updateBookFileHeader(true);
-    updateISBNIndex(book.ISBN, newSize, true);
-    cout << "\nBook added successfully.\n\n";
-
-    // Flush and close the file
-    bookFile.flush();
-    bookFile.close();
-}
-
-void SLS::updateAuthorName()
-{
-}
-
-void SLS::updateBookTitle()
-{
-}
-
-void SLS::deleteBook()
-{
-}
-
-void SLS::printAuthor()
-{
-}
-
-void SLS::printBook()
-{
-    bookFile.open("Books.txt", ios::in);
-    vector<Book> books;
-
-    bookFile.seekg (0,ios::end);
-    long long size = bookFile.tellg();
-    bookFile.seekg(0);
-
-    int n = size / 60;
-//    cout<<n<<endl;
-
-    for(int i=0;i<n;i++) {
-        bookFile.read((char *) &book, sizeof(book));
-        books.push_back(book);
-    }
-
-    for(Book b:books){
-        cout<<"ISBN: "<<b.ISBN<<endl;
-        cout<<"Book title: "<<b.title<<endl;
-        cout<<"book author ID: "<<b.authorID<<endl;
-        cout<<"------------------"<<endl;
-    }
-}
-
-void SLS::query()
-{
-}
-
-void SLS::updateISBNIndex(char isbn[15], int rrn, bool flag) {
-    map<char*, int> primaryI;
-
-    primaryIFile.open("ISBNIndex.txt", ios::app | ios::in);
-    primaryIFile.seekg (0,ios::end);
-    long long size = primaryIFile.tellg();
-    primaryIFile.seekg(0);
-
-    int n = size / 18;
-    if(flag)                    //addition
-    {
-        if(primaryIFile.eof())
-        {
-            primaryIFile << isbn << '|';
-            primaryIFile << rrn << '\n';
-            return;
-        }
-        primaryI.insert({isbn, rrn});
-
-        for(int i = 0; i < n; i++) {
-            primaryIFile.read(isbn,'|');
-            primaryIFile >> rrn;
-            primaryI.insert({isbn, rrn});
-            primaryIFile.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-        for(auto i: primaryI)
-        {
-            primaryIFile << i.first << '|';
-            primaryIFile << i.second << '\n';
-        }
-    }
-    primaryIFile.close();
-}
-
-int SLS::updateBookFileHeader(bool flag) {
-    if(flag )                                // addition
-    {
-        ifstream bookfile("Books.txt");
-        int size = 0;
-        string firstIntegerStr;
-
-        // Read the first string until the "|" delimiter
-        if (getline(bookfile, firstIntegerStr, '|')) {
-            // Convert the string to an integer
-            int firstInteger = stoi(firstIntegerStr);
-
-            // Print or process the first integer
-            std::cout << "First Integer: " << firstInteger << std::endl;
-            size = firstInteger;
-        }
-        size++;
-        firstIntegerStr = to_string(size);
-        bookFile.seekp(0);
-//        bookFile.write((char*)&firstIntegerStr,);
-        return size;
-    }
-
-}
-
+//    authorFile.open("file.txt" , ios::app);
+//    authorFile.write((char*)author.ID , strlen(author.ID));
+//    authorFile << '|';
+//    authorFile.write((char *) author.name , strlen(author.name));
+//    authorFile << '|';
+//    authorFile.write((char *) author.address , strlen(author.address));
+//    authorFile << "\n";
+//    insertAuthorIndex(recsize , author.ID);
+//    recsize+=strlen(author.ID) + strlen(author.name) + strlen(author.address) + 3;
+//    authorFile.close();
+//}
+//void SLS::addBook()
+//{
+//    bookFile.open("Books.txt", ios::out | ios:: binary | ios::app | ios::in);
+//    // Check if the file is open
+//    if (!bookFile.is_open()) {
+//        cout << "Error opening file\n";
+//        return;
+//    }
+//
+////    if (bookFile.peek() == EOF)
+////    {
+////        char* size = "0";
+////        char* AVAIL = "-1";
+////        bookFile.write(size,sizeof (size));
+////        bookFile << "|";
+////        bookFile.write(AVAIL,sizeof (AVAIL));
+////        bookFile << "\n";
+////
+////    }
+//
+//    char isbn[15], t[30], aid[15];
+//    cin.ignore();
+//    cout << "Enter book's ISBN:";
+//    cin >> isbn;
+//    cout << "Enter book's title:";
+//    cin >> t;
+//    cout << "Enter book's author's ID:";
+//    cin >> aid;
+//
+//    this->book = Book(isbn, t, aid);
+//
+//    // Write data as binary structure
+//    bookFile.seekp(0, ios::end);
+//    bookFile.write((char*)(&book), sizeof(book));
+//
+//
+//    int newSize = updateBookFileHeader(true);
+//    updateISBNIndex(book.ISBN, newSize, true);
+//    cout << "\nBook added successfully.\n\n";
+//
+//    // Flush and close the file
+//    bookFile.flush();
+//    bookFile.close();
+//}
+//
+//void SLS::updateAuthorName()
+//{
+//}
+//
+//void SLS::updateBookTitle()
+//{
+//}
+//
+//void SLS::deleteBook()
+//{
+//}
+//
+//
+//void SLS::printBook()
+//{
+//    bookFile.open("Books.txt", ios::in);
+//    vector<Book> books;
+//
+//    bookFile.seekg (0,ios::end);
+//    long long size = bookFile.tellg();
+//    bookFile.seekg(0);
+//
+//    int n = size / 60;
+////    cout<<n<<endl;
+//
+//    for(int i=0;i<n;i++) {
+//        bookFile.read((char *) &book, sizeof(book));
+//        books.push_back(book);
+//    }
+//
+//    for(Book b:books){
+//        cout<<"ISBN: "<<b.ISBN<<endl;
+//        cout<<"Book title: "<<b.title<<endl;
+//        cout<<"book author ID: "<<b.authorID<<endl;
+//        cout<<"------------------"<<endl;
+//    }
+//}
+//
+//void SLS::query()
+//{
+//}
+//
+//void SLS::updateISBNIndex(char isbn[15], int rrn, bool flag) {
+//    map<char*, int> primaryI;
+//
+//    primaryIFile.open("ISBNIndex.txt", ios::app | ios::in);
+//    primaryIFile.seekg (0,ios::end);
+//    long long size = primaryIFile.tellg();
+//    primaryIFile.seekg(0);
+//
+//    int n = size / 18;
+//    if(flag)                    //addition
+//    {
+//        if(primaryIFile.eof())
+//        {
+//            primaryIFile << isbn << '|';
+//            primaryIFile << rrn << '\n';
+//            return;
+//        }
+//        primaryI.insert({isbn, rrn});
+//
+//        for(int i = 0; i < n; i++) {
+//            primaryIFile.read(isbn,'|');
+//            primaryIFile >> rrn;
+//            primaryI.insert({isbn, rrn});
+//            primaryIFile.ignore(numeric_limits<streamsize>::max(), '\n');
+//        }
+//        for(auto i: primaryI)
+//        {
+//            primaryIFile << i.first << '|';
+//            primaryIFile << i.second << '\n';
+//        }
+//    }
+//    primaryIFile.close();
+//}
+//
+//int SLS::updateBookFileHeader(bool flag) {
+//    if(flag )                                // addition
+//    {
+//        ifstream bookfile("Books.txt");
+//        int size = 0;
+//        string firstIntegerStr;
+//
+//        // Read the first string until the "|" delimiter
+//        if (getline(bookfile, firstIntegerStr, '|')) {
+//            // Convert the string to an integer
+//            int firstInteger = stoi(firstIntegerStr);
+//
+//            // Print or process the first integer
+//            std::cout << "First Integer: " << firstInteger << std::endl;
+//            size = firstInteger;
+//        }
+//        size++;
+//        firstIntegerStr = to_string(size);
+//        bookFile.seekp(0);
+////        bookFile.write((char*)&firstIntegerStr,);
+//        return size;
+//    }
+//
+//}
